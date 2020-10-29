3de75352628e1562ad5c9f5367190cce6d525e00
mrrukkus <doooog1@rambler.ru>
2020-10-29 14:11:16 +0500

    Удален старый код

--- src/components/Task/task.jsx
+++ src/components/Task/task.jsx
@@ -1,60 +1,6 @@
 import React, {Component} from 'react';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
-const TaskFunc = ({taskState}) => {
-  switch (taskState) {
-    case ("completed"):
-      return (
-        <>
-          <li className="completed">
-            <div className="view">
-              <input className="toggle" type="checkbox"/>
-              <label>
-                <span className="description">Completed task</span>
-                <span className="created">{formatDistanceToNow(new Date(2020, 6, 2), { includeSeconds: true, addSuffix: true })}</span>
-              </label>
-              <button className="icon icon-edit"></button>
-              <button className="icon icon-destroy"></button>
-            </div>
-          </li>
-        </>
-      )
-    case ("editing"):
-      return (
-        <>
-          <li className="editing">
-            <div className="view">
-              <input className="toggle" type="checkbox"/>
-              <label>
-                <span className="description">Editing task</span>
-                <span className="created">{formatDistanceToNow(new Date(2020, 6, 2), { includeSeconds: true, addSuffix: true })}</span>
-              </label>
-              <button className="icon icon-edit"></button>
-              <button className="icon icon-destroy"></button>
-            </div>
-            <input type="text" className="edit" defaultValue="Editing task"/>
-          </li>
-        </>
-      )
-    case ("view"):
-      return (
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox"/>
-            <label>
-              <span className="description">Active task</span>
-              <span className="created">{formatDistanceToNow(new Date(2020, 9, 29), { includeSeconds: true, addSuffix: true })}</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-          </div>
-        </li>
-      )
-      default:
-        return null;
-  }
-};
-
 export default class Task extends Component {
   constructor() {
     super();
