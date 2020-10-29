ba8b323b7f5a7f7dafa04c8e6ced9026b5155c37
mrrukkus <doooog1@rambler.ru>
2020-10-29 15:25:09 +0500

    Добавлена реализация изменения состояния задачи активный-выполненный

--- src/components/Task/task.jsx
+++ src/components/Task/task.jsx
@@ -22,20 +22,33 @@ export default class Task extends Component {
   }
 
   render() {
+    const getTaskClassName = () => {
+      if (this.state.isDone) {
+        return `completed`
+      } else if (this.state.isEdit) {
+        return `editing`
+      }
+      return ``;
+    };
+
+    console.log(getTaskClassName());
+    const taskDescription = this.state.isDone ? "Completed" : "Active";
+    const editInput = this.state.isEdit && (
+      <input type="text" className="edit" defaultValue="Editing task"/>
+      )
+
     return (
-      <li className={this.state.isDone ? "completed" : "view"}>
+      <li className={getTaskClassName()}>
         <div className="view">
           <input className="toggle" type="checkbox" onClick={this.onCheckboxClick}/>
           <label>
-            <span className="description">{this.state.isDone ? "Completed" : "Active"} task</span>
+            <span className="description">{taskDescription} task</span>
             <span className="created">{formatDistanceToNow(new Date(2020, 6, 2), { includeSeconds: true, addSuffix: true })}</span>
           </label>
           <button className="icon icon-edit"></button>
           <button className="icon icon-destroy"></button>
-          {this.state.isEdit && (
-          <input type="text" className="edit" defaultValue="Editing task"/>
-          )}
         </div>
+        {editInput}
       </li>
     )
   }
