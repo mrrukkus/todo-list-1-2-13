d5e9da3371a50cea255dcdf7234373e2c5d2e056
mrrukkus <doooog1@rambler.ru>
2020-10-29 14:09:06 +0500

    Изменен компонент Task на классовый компонент, изменена логика отрисовки разметки компонента в зависимости от состояния. Добавлен обработчик на изменение состояния при клике на чекбокс

--- src/components/Task/task.jsx
+++ src/components/Task/task.jsx
@@ -1,6 +1,7 @@
+import React, {Component} from 'react';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
-const Task = ({taskState}) => {
+const TaskFunc = ({taskState}) => {
   switch (taskState) {
     case ("completed"):
       return (
@@ -54,4 +55,42 @@ const Task = ({taskState}) => {
   }
 };
 
-export default Task;
+export default class Task extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isDone: false,
+      isEdit: false
+    };
+
+    this.onCheckboxClick = this.onCheckboxClick.bind(this);
+  }
+
+  onCheckboxClick() {
+    this.setState((state) => {
+      return {
+        isDone: !state.isDone
+      }
+    });
+  }
+
+  render() {
+    return (
+      <li className={this.state.isDone ? "completed" : "view"}>
+        <div className="view">
+          <input className="toggle" type="checkbox" onClick={this.onCheckboxClick}/>
+          <label>
+            <span className="description">{this.state.isDone ? "Completed" : "Active"} task</span>
+            <span className="created">{formatDistanceToNow(new Date(2020, 6, 2), { includeSeconds: true, addSuffix: true })}</span>
+          </label>
+          <button className="icon icon-edit"></button>
+          <button className="icon icon-destroy"></button>
+          {this.state.isEdit && (
+          <input type="text" className="edit" defaultValue="Editing task"/>
+          )}
+        </div>
+      </li>
+    )
+  }
+}
